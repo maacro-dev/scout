46ea9586d2c050548015535bb38114cea6e48934
maomao <meladryyy@gmail.com>
2025-06-28 03:12:06 +0800

    Add UserData data class for user preferences

---
+++ core/model/src/main/java/com/maacro/scout/core/model/UserData.kt
@@ -0,0 +1,11 @@
+package com.maacro.scout.core.model
+
+/**
+ * TODO
+ * Class summarizing user data
+ *
+ */
+data class UserData(
+    val themeConfig: ThemeConfig,
+    val shouldHideOnboarding: Boolean,
+)

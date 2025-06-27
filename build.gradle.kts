6b53106229bf2333a01a4358f61e3e741fbafd07
maomao <meladryyy@gmail.com>
2025-06-27 23:39:14 +0800

    Update root build.gradle.kts to support modular architecture

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,17 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+	repositories {
+		google {
+			content {
+				includeGroupByRegex("com\\.android.*")
+				includeGroupByRegex("com\\.google.*")
+				includeGroupByRegex("androidx.*")
+			}
+		}
+		mavenCentral()
+	}
+}
+
 plugins {
 	alias(libs.plugins.android.application) apply false
 	alias(libs.plugins.kotlin.android) apply false
@@ -6,4 +19,7 @@ plugins {
 	alias(libs.plugins.hilt) apply false
 	alias(libs.plugins.kotlin.serialization) apply false
 	alias(libs.plugins.ksp) apply false
+	alias(libs.plugins.room) apply false
+    alias(libs.plugins.android.library) apply false
+	alias(libs.plugins.kotlin.jvm) apply false
 }
